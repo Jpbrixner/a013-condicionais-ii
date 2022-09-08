bb89a537b13f789eaa89b1c68334c20e916350dd
Jpbrixner <jpbrixner1993@gmail.com>
2022-09-08 16:29:39 -0300

    Exercicio Completo

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,25 @@
-const num = //seu numero aqui
+//Crie um código que receba um número por **prompt** e verifique se um número é divisível por 2 **ou** por 3
+//Faça isso:
+//1. Utilizando ifs aninhados
 
+const num = +prompt("Digite um numero");
+
+if (num % 2 === 0) {
+  if (num % 3 === 0) {
+    console.log("o numero é divisivel por 2 e 3");
+  } else {
+    console.log("o numero é divisivel por 2 e nao por 3");
+  }
+} else if (num % 3 === 0) {
+  console.log("o numero é divisivel por 3 e não por 2");
+} else {
+  console.log("o numero não é dividido por 2 e 3");
+}
+
+//2. Utilizando um operador lógico para unir duas operações relacionais
+
+if (num % 2 === 0 || num % 3 === 0) {
+  console.log("o numero é divisivel por 2 ou 3");
+} else {
+  console.log("Não é divisivel por 2 ou3");
+}
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,13 +1,27 @@
+//Reescreva os códigos do arquivo script.js utilizando o if ternário.
+
 //código a ser reescrito
 
-if(nome === "José"){
-	console.log("Oi, Zé!");
+/*if (nome === "José") {
+  console.log("Oi, Zé!");
 } else {
-	console.log("Olá, "+nome);
+  console.log("Olá, " + nome);
 }
 
-if(idade >= 18){
-	console.log("pode tirar carteira de motorista!");
+if (idade >= 18) {
+  console.log("pode tirar carteira de motorista!");
 } else {
-	console.log("Ainda não pode tirar carteira de motorista");
-}
+  console.log("Ainda não pode tirar carteira de motorista");
+} */
+
+//resposta:
+const nome = String(prompt("Qual é o seu nome?")).toLowerCase();
+const idade = +prompt("Qual é a sua idade?");
+
+console.log(nome === "josé" ? "Oi, Zé!" : "Olá," + nome);
+
+console.log(
+  idade >= 18
+    ? "pode tirar carteira de motorista!"
+    : "Ainda não pode tirar carteira de motorista"
+);
